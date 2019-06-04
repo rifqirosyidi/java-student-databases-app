796ca6b49fc664f6350c36d61909c6dea14fa8be
Rifqi <rief.rosyidi@gmail.com>
2019-06-04 12:45:23 +0700

    create array of object

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,16 +1,24 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		// Ask how many users want to add
-		Student stu1 = new Student();
-		stu1.enroll();
-		stu1.viewBalance();
-		stu1.payTuition();
-		System.out.println(stu1.showInfo());
+		System.out.print("How many students you want to enroll :");
+		Scanner in = new Scanner(System.in);
+		int numOfStudent = in.nextInt();
+		Student[] students = new Student[numOfStudent];
 		
-		// Create number of new student
+		// Create i number of new student
+		for (int i = 0; i < numOfStudent; i++) {
+			students[i] = new Student();
+			students[i].enroll();
+			students[i].viewBalance();
+			students[i].payTuition();
+			System.out.println(students[i].showInfo());
+		}
 
 	}
 
